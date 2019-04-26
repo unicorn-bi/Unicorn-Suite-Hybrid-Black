baa946aeace39acfab5cfde0b4501683f37a6bd5
Unicorn - The Brain Interface <walchshofer@gtec.at>
2019-04-26 14:37:11 +0200

    added speller-unity interface

---
+++ Unicorn Speller/Unity/Assets/UnicornSpellerInterface/SpellerReceiver.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using Intendix.Board;
+
+namespace Unity.ItemRecever
+{
+    public class ItemReceivedEventArgs : EventArgs
+    {
+        public BoardItem BoardItem { get; set; }
+    }
+
+    public class SpellerReceiver : ItemReceiver.ItemReceiver
+    {
+        #region Events...
+
+        /// <summary>
+        /// This method is called if a classified item is received from Unicorn Speller.
+        /// </summary>
+        public event EventHandler OnItemReceived;
+
+        #endregion
+
+        #region Private members...
+
+        private int _port = 0;
+        private IPAddress _ip = null;
+        private ItemReceivedEventArgs _eventArgs;
+
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SpellerReceiver"/>. 
+        /// </summary>
+        /// <param name="ip">The <see cref="SpellerReceiver"/> is listening for network messages on this ip address.</param>
+        /// <param name="port">The <see cref="SpellerReceiver"/> is listening for network messages on this port.</param>
+        public SpellerReceiver(IPAddress ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+            _eventArgs = new ItemReceivedEventArgs();
+            Start();
+        }
+
+        /// <summary>
+        /// This method is called if a classified item is received from Unicorn Speller.
+        /// </summary>
+        /// <param name="item"><see cref="BoardItem"/> represents one item of the speller board.</param>
+        public override void ItemReceived(BoardItem item)
+        {
+            _eventArgs.BoardItem = item;
+            OnItemReceived(this, _eventArgs);
+        }
+
+        /// <summary>
+        /// Starts listening for network messages from speller on the defined ip and port.
+        /// </summary>
+        public void Start()
+        {
+            if (!IsRunning)
+            {
+                BeginReceiving(_ip, _port);
+            }
+            else
+                throw new InvalidOperationException(String.Format("Receiver is already running."));
+        }
+
+        /// <summary>
+        /// Stopps listening for network messages from speller.
+        /// </summary>
+        public void Stop()
+        {
+            if (IsRunning)
+            {
+                BeginReceiving(_ip, _port);
+            }
+            else
+                throw new InvalidOperationException(String.Format("Receiver is not running yet."));
+        }
+    }
+}
---
+++ Unicorn Speller/Unity/Assets/UnicornSpellerInterface/UnicornSpellerInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using System.Net;
+using Unity.ItemRecever;
+
+public class UnicornSpellerInterface : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            //IP settings
+            string ips = "127.0.0.1";
+            int port = 1000;
+            IPAddress ip = IPAddress.Parse(ips);
+        
+            //Start listening for Unicorn Speller network messages
+            SpellerReceiver r = new SpellerReceiver(ip, port);
+
+            //attach items received event
+            r.OnItemReceived += OnItemReceived;
+
+            Debug.Log(String.Format("Listening to {0} on port {1}.", ip, port));
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Do something...
+    }
+
+    // OnItemReceived is called if a classified item is received from Unicorn Speller via udp.
+    private void OnItemReceived(object sender, EventArgs args)
+    {
+        ItemReceivedEventArgs eventArgs = (ItemReceivedEventArgs) args;
+        Debug.Log(String.Format("Received BoardItem:\tName: {0}\tOutput Text: {1}", eventArgs.BoardItem.Name, eventArgs.BoardItem.OutputText));
+
+        //Do something...
+    }
+}
